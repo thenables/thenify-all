0a97803458c2e41450609d7c16002d4b401c5b6f
dead-horse dead_horse@qq.com <busi.hyy@taobao.com>
2014-12-19 17:45:01 +0800

    support promisifyAll(source, [obj], [methods])

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 var thenify = require('thenify')
 
 module.exports = function (source, destination, methods) {
+  if (!destination) {
+    destination = {};
+    methods = Object.keys(source)
+  }
+
+  if (Array.isArray(destination)) {
+    methods = destination
+    destination = {}
+  }
+
+  if (!methods) {
+    methods = Object.keys(source)
+  }
+
   methods.forEach(function (name) {
     // promisify only if it's a function
     if (typeof source[name] === 'function') destination[name] = thenify(name, source[name])
