765a015008774903d3b2210b00bf78567cfe5b79
Charlike Mike Reagent <mamemto_100@mail.bg>
2014-12-23 18:10:52 +0200

    add check if `source` is function, then thenify it

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ module.exports = function (source, destination, methods) {
     methods = Object.keys(source)
   }
 
+  if (typeof source === 'function') destination = thenify(source)
+
   methods.forEach(function (name) {
     // promisify only if it's a function
     if (typeof source[name] === 'function') destination[name] = thenify(source[name])
