b0488960ad090e8b90a23b874762e50942dbbdf8
Jonathan Ong <jonathanrichardong@gmail.com>
2014-12-25 00:18:01 -0800

    bump thenify

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = function (source, destination, methods) {
 
   methods.forEach(function (name) {
     // promisify only if it's a function
-    if (typeof source[name] === 'function') destination[name] = thenify(name, source[name])
+    if (typeof source[name] === 'function') destination[name] = thenify(source[name])
   })
 
   // proxy the rest
